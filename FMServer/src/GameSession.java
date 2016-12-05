f55ef00b695e35533c258267296c0607c39be6c9
acevedd <acevedd@purdue.edu>
2016-12-04 20:31:44 -0500

    Update GameSession.java

--- FMServer/src/GameSession.java
+++ FMServer/src/GameSession.java
@@ -1,21 +1,30 @@
 import java.net.*;
 import java.io.*;
+import java.util.HashMap;
 
 public class GameSession extends Thread{
 	private User gameLeader;
-	private User gameParticipants;
-	final String gameToken;
+	private Socket clientSocket;
+	private BufferedReader reader;
+	private PrintWriter writer;
+	private boolean start;
+	final String gameKey;
 	static int minPlayers = 2;
+	static HashMap<String, User> currentParticipants= new HashMap<>();
 	
-	public GameSession(User gameLeader, String gameToken){
+	public GameSession(User gameLeader, String gameKey, PrintWriter writer, BufferedReader reader, Socket clientSocket){
 		this.gameLeader = gameLeader;
-		this.gameToken = gameToken;
+		this.gameKey = gameKey;
+		this.writer = writer;
+		this.reader = reader;
+		this.clientSocket = clientSocket;
 	}
 	
 	//define run() , probably should run when game leader specifies some boolean start = true;
 	public void run(){
 		try{
-			while(true){
+			start = true;
+			while(start){
 				gameStart();
 			}
 		}catch(IOException e){
@@ -24,6 +33,30 @@ public class GameSession extends Thread{
 	}
 	
 	public void gameStart()throws IOException{
+		writer.println("Waiting for participants");
+		try{
+			String inputLine;
+			while((inputLine = reader.readLine()) != null){
+				
+				
+			}
+			
+		}catch(IOException e){
+			System.out.println("Unexpected interruption");
+			clientSocket.close();
+			start = false;
+		}finally{
+			if(writer != null){
+				writer.close();
+			}
+			if(reader != null){
+				reader.close();
+			}
+			clientSocket.close();
+			start = false;
+		}
+			
+	
 		
 	}
 }
