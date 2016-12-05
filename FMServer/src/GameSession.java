f6bf3ccb6a9f48993041f36aea2cdf2f6a362966
acevedd <acevedd@purdue.edu>
2016-12-05 17:34:25 -0500

    Update GameSession.java

--- FMServer/src/GameSession.java
+++ FMServer/src/GameSession.java
@@ -2,68 +2,30 @@ import java.net.*;
 import java.io.*;
 import java.util.HashMap;
 
-public class GameSession extends Thread{
+public class GameSession{
 	public User gameLeader;
+	
 	private Socket clientSocket;
 	private BufferedReader reader;
 	private PrintWriter writer;
+	private static BufferedReader inWordleDeck = null;
 	private boolean start;
 	final String gameKey;
 	static int minPlayers = 2;
 	private int numPlayers = 0;
 	private HashMap<String, User> currentParticipants= new HashMap<>(); //Username is key
 	
-	public GameSession(User user, String gameKey){
+	public GameSession(User user, String gameKey, Socket clientSocket, PrintWriter writer, BufferedReader reader){
 		this.gameKey = gameKey;
 		this.currentParticipants.put(user.getUsername(), user);
+		this.clientSocket = clientSocket;
+		this.writer = writer;
+		this.reader = reader;
 		this.numPlayers++;
 	}
 	
 	public void setGameLeader(User gameLeader){
 		this.gameLeader = gameLeader;
-	}
-	
-	public void run(){
-		
-		
-		try{
-			writer = new PrintWriter(FMServer.runningSockets.get(gameLeader.getID()).getOutputStream(), true);
-			reader = new BufferedReader(new InputStreamReader(FMServer.runningSockets.get(gameLeader.getID()).getInputStream()));
-			
-			if(minPlayers <= numPlayers)
-				writer.println("NEWPARTICIPANTS--");
-				
-			while(minPlayers <= numPlayers){
-				gameStart();
-			}
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-	
-	public void gameStart()throws IOException{
-		try{
-			String inputLine;
-			while((inputLine = reader.readLine()) != null){
-				
-				
-			}
-			
-		}catch(IOException e){
-			System.out.println("Unexpected interruption");
-			clientSocket.close();
-			start = false;
-		}finally{
-			if(writer != null){
-				writer.close();
-			}
-			if(reader != null){
-				reader.close();
-			}
-			clientSocket.close();
-			start = false;
-		}
-		
 	}
 	
 	public HashMap<String, User> getCurrentParticipants(){
@@ -74,5 +36,10 @@ public class GameSession extends Thread{
 		this.numPlayers++;
 	}
 	
-
+	public void writePlayer(String message){
+		this.writer.println(message);
+	}
+	
+	
+	
 }
