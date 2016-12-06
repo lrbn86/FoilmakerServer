0c67d01ff2119700bebefc9e37337ed4a0c75ff1
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-12-05 19:25:13 -0500

    Update GameSession.java

--- FMServer/src/GameSession.java
+++ FMServer/src/GameSession.java
@@ -1,62 +1,42 @@
 import java.net.*;
 import java.io.*;
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class GameSession extends Thread{
-	private User gameLeader;
-	private Socket clientSocket;
-	private BufferedReader reader;
-	private PrintWriter writer;
-	private boolean start;
-	final String gameKey;
-	static int minPlayers = 2;
-	static HashMap<String, User> currentParticipants= new HashMap<>();
-	
-	public GameSession(User gameLeader, String gameKey, PrintWriter writer, BufferedReader reader, Socket clientSocket){
-		this.gameLeader = gameLeader;
-		this.gameKey = gameKey;
-		this.writer = writer;
-		this.reader = reader;
-		this.clientSocket = clientSocket;
-	}
-	
-	//define run() , probably should run when game leader specifies some boolean start = true;
-	public void run(){
-		try{
-			start = true;
-			while(start){
-				gameStart();
-			}
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-	
-	public void gameStart()throws IOException{
-		writer.println("Waiting for participants");
-		try{
-			String inputLine;
-			while((inputLine = reader.readLine()) != null){
-				
-				
-			}
-			
-		}catch(IOException e){
-			System.out.println("Unexpected interruption");
-			clientSocket.close();
-			start = false;
-		}finally{
-			if(writer != null){
-				writer.close();
-			}
-			if(reader != null){
-				reader.close();
-			}
-			clientSocket.close();
-			start = false;
-		}
-			
-	
-		
-	}
+public class GameSession{
+    public User gameLeader;
+
+    private Socket clientSocket;
+    private BufferedReader reader;
+    private PrintWriter writer;
+    private static BufferedReader inWordleDeck = null;
+    private boolean start;
+    final String gameKey;
+    static AtomicInteger numPlayers = new AtomicInteger();
+    private HashMap<String, User> currentParticipants= new HashMap<>(); //Username is key
+
+    public GameSession(User user, String gameKey, Socket clientSocket, PrintWriter writer, BufferedReader reader){
+        this.gameKey = gameKey;
+        this.currentParticipants.put(user.getUsername(), user);
+        this.clientSocket = clientSocket;
+        this.writer = writer;
+        this.reader = reader;
+    }
+
+    public void setGameLeader(User gameLeader){
+        this.gameLeader = gameLeader;
+    }
+
+    public HashMap<String, User> getCurrentParticipants(){
+        return this.currentParticipants;
+    }
+
+    public int incNumPlayers() {
+        return numPlayers.incrementAndGet();
+    }
+
+    public void writePlayer(String message){
+        this.writer.println(message);
+    }
+
 }
