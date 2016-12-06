f1fcec2c23a44210142cf6547fb9da6a2edae625
Brandon Nguyen <lrbn@users.noreply.github.com>
2016-12-05 19:21:39 -0500

    Update ClientHandler.java

--- FMServer/src/ClientHandler.java
+++ FMServer/src/ClientHandler.java
@@ -1,77 +1,91 @@
 import java.net.*;
 import java.io.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class ClientHandler extends Thread{
-	private Socket clientSocket;
-	private int clientID;
-	private String gameToken;
-	private boolean running;
-	
-	public ClientHandler(Socket clientSocket, int clientID){
-		this.clientSocket = clientSocket;
-		this.clientID = clientID;
-	}
-	
-	public void run(){
-		try{
-			running = true;
-			while(running){	
-				clientInteraction();
-			}
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-	}
-	
-	public void clientInteraction()throws IOException{
-		PrintWriter outToClient = null;
-		BufferedReader inFromClient = null;
-		String inputLine, outputLine = null;
-		
-		try{
-			outToClient = new PrintWriter(clientSocket.getOutputStream(), true);
-			inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-			
-			//implement while loop that runs while client is connected, this will handle requests
-			while((inputLine = inFromClient.readLine()) != null){
-				System.out.println("Request from client: " + inputLine);
-				String[] arr = inputLine.split("--");
-				
-				if(arr[0].equals("CREATENEWUSER")){
-					outputLine = FMServer.registerUser(inputLine);
-					outToClient.println(outputLine);
-					System.out.println("Sent to client: " + outputLine);
-				}else if(arr[0].equals("LOGIN")){
-					outputLine = FMServer.loginUser(inputLine);
-					outToClient.println(outputLine);
-					String[] arr2 = outputLine.split("--");
-					this.gameToken = arr2[3];
-					System.out.println("Sent to client: " + outputLine);
-				}else if(arr[0].equals("STARTNEWGAME")){
-					//create a game session object with the current user as leader and user's token
-					outputLine = FMServer.startNewGame(inputLine, outToClient, inFromClient, clientSocket);
-					outToClient.println(outputLine);
-				}
-			}
-			
-		}catch(IOException e){
-			System.out.println("Unexpected interruption");
-			FMServer.currentUserHash.remove(this.gameToken);
-			closeSocket();
-		}finally{
-			if(outToClient != null)
-				outToClient.close();
-			if(inFromClient != null)
-				inFromClient.close();
-				System.out.println("User " + clientID + " disconnected");
-			clientSocket.close();
-			FMServer.currentUserHash.remove(this.gameToken);
-			closeSocket();
-			}
-		}
-	
-	public void closeSocket(){
-		running = false;
-	}
+    private Socket clientSocket;
+    private int clientID;
+    private String gameToken;
+    private boolean running;
+    private AtomicInteger numOfSuggestions = new AtomicInteger();
+
+    public ClientHandler(Socket clientSocket, int clientID){
+        this.clientSocket = clientSocket;
+        this.clientID = clientID;
+    }
+
+    public void run(){
+        try{
+            running = true;
+            while(running){
+                clientInteraction();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void clientInteraction()throws IOException{
+        PrintWriter outToClient = null;
+        BufferedReader inFromClient = null;
+        String inputLine, outputLine = null;
+
+        try{
+            outToClient = new PrintWriter(clientSocket.getOutputStream(), true);
+            inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            //implement while loop that runs while client is connected, this will handle requests
+            while((inputLine = inFromClient.readLine()) != null){
+                System.out.println("Request from client: " + inputLine);
+                String[] arr = inputLine.split("--");
+
+                if(arr[0].equals("CREATENEWUSER")){
+                    outputLine = FMServer.registerUser(inputLine);
+                    outToClient.println(outputLine);
+                    System.out.println("Sent to client: " + outputLine);
+                }else if(arr[0].equals("LOGIN")){
+                    outputLine = FMServer.loginUser(inputLine, this.clientID);
+                    outToClient.println(outputLine);
+                    
+                    String[] arr2 = outputLine.split("--");
+                    // TODO An ArrayIndexOutOfBoundsException occurs if user attempts to login with new credentials without registering into database first
+                    // The client sends RESPONSE--LOGIN--UNKNOWNUSER
+                    this.gameToken = arr2[3];
+                    
+                    System.out.println("Sent to client: " + outputLine);
+                }else if(arr[0].equals("STARTNEWGAME")){
+                    outputLine = FMServer.startNewGame(inputLine, outToClient, inFromClient, this.clientSocket);
+                    outToClient.println(outputLine);
+                }else if(arr[0].equals("JOINGAME")){
+                    outputLine = FMServer.joinGame(inputLine, outToClient, inFromClient, this.clientSocket);
+                    outToClient.println(outputLine);
+                }else if(arr[0].equals("ALLPARTICIPANTSHAVEJOINED")){
+                    FMServer.sessionStart(inputLine);
+                } else if (arr[0].equals("PLAYERSUGGESTION")) {
+                    System.out.println(GameSession.numPlayers);
+                }
+            }
+
+        }catch(IOException e){
+            System.out.println("Unexpected interruption");
+            FMServer.currentUserHash.remove(this.gameToken);
+            closeSocket();
+        }finally{
+            if(outToClient != null)
+                outToClient.close();
+            if(inFromClient != null)
+                inFromClient.close();
+            System.out.println("User " + clientID + " disconnected");
+            clientSocket.close();
+            FMServer.currentUserHash.remove(this.gameToken);
+            closeSocket();
+        }
+    }
+
+
+
+    public void closeSocket(){
+        running = false;
+    }
 }
